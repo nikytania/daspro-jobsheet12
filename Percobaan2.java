e538e9c431be22dc9c232aadb73fb87640d4e2aa
nikytania <you@example.com>
2024-11-29 08:18:22 +0700

    jobsheet 12 niky

--- Percobaan2.java
+++ Percobaan2.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Percobaan2 {
   static int hitungPangkat(int x, int y) {
     if (y == 0) {
-        return (1);
+        System.out.print("1"); 
+        return 1;
     } else {
+        System.out.print(x + " * ");
         return x * hitungPangkat(x, y - 1);
     }
 }
@@ -12,12 +14,15 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     int bilangan, pangkat;
-
+    
     System.out.print("Masukkan bilangan yang dihitung: ");
     bilangan = sc.nextInt();
     System.out.print("Masukkan pangkat: ");
     pangkat = sc.nextInt();
 
-    System.out.println(hitungPangkat(bilangan, pangkat));
+    System.out.print("Deret perhitungan: ");
+    int hasil = hitungPangkat(bilangan, pangkat);
+    System.out.println(" = " + hasil);
+
   }
 }
